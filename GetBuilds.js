90db93ce96342c91c46dd92ddca000abfc986d31
Sennevds <senne.vandesompele@gmail.com>
2014-04-02 10:21:15 +0200

    read file in GetBuilds

--- GetBuilds.js
+++ GetBuilds.js
@@ -42,15 +42,15 @@ function getPoints(buildId, Person) {
     ],
 // optional callback
         function (err, results) {
-            for (var j = 0; j < results.length; j++) {
-
-
-                if (j == 0) {
-                    buildstatus[j] = new buildStatus(results[j]['buildType']['name'], results[j]['status'], results[j]['buildType']['id'], results[j]['startDate'], results[j]['finishDate']);
-                }
+            var tempfile= JSON.parse(fileio.readFile("temp/score.txt"));
+            for (var i = 0; i < tempfile.length; i++) {
+                usersarray[i]=new person(tempfile[i]['name']);
+                usersarray[i].lastdate = tempfile[i]['lastdate'];
+                usersarray[i].build = tempfile[i]['build'];
+                usersarray[i].gravUrl = tempfile[i]['gravUrl'];
+                usersarray[i].points = tempfile[i]['points'];
             }
-
-
+            buildstatus[0] = new buildStatus(results[0]['buildType']['name'], results[0]['status'], results[0]['buildType']['id'], results[0]['startDate'], results[0]['finishDate']);
 
             for (var k = 0; k < buildstatus.length; k++) {
                 if (!usersarray[k] && results[2] != null) {
@@ -72,19 +72,19 @@ function getPoints(buildId, Person) {
 
 
             }
-            userInfo( function(er2, profiles) {
-            for (var user = 0; user < usersarray.length; user++) {
-                for (var i = 0; i < profiles.length; i++) {
-                    if (usersarray[user]['name'] == profiles[i]['username']) {
-                        usersarray[user].gravUrl = profiles[i]['img'];
+            userInfo(function (er2, profiles) {
+                for (var user = 0; user < usersarray.length; user++) {
+                    for (var i = 0; i < profiles.length; i++) {
+                        if (usersarray[user]['name'] == profiles[i]['username']) {
+                            usersarray[user].gravUrl = profiles[i]['img'];
+                        }
                     }
                 }
-            }
-            fileio.writeFile("temp/score.txt",JSON.stringify(usersarray));
-            Person(err, usersarray);
-        });
-            
-            
+                fileio.writeFile("temp/score.txt", JSON.stringify(usersarray));
+                Person(err, usersarray);
+            });
+
+
         });
     //setTimeout(getPoints, 5 * 1000);
 }
