dc178ccd25e78b421ce2fb54aa2cbd0048c94c88
senne <senne.vandesompele@gmail.com>
2014-04-22 14:50:44 +0200

    added function to check names

---
+++ checkUsername.js
@@ -0,0 +1,20 @@
+﻿var GetData = require('./Request');
+var async = require('async');
+
+function findDouble(name) {
+ 
+            if(name == "rakhmetovruslan"){
+                return "ruslan";
+                }
+            if(name == "cybercaveman"){
+                return "wouter";
+                }
+            if(name == "bobvanlooveren" || name == "bubbltrubl"){
+                return "bob";
+                }
+            else{
+                 return name;
+                }
+        
+};
+module.exports = findDouble;
---
+++ obj/Debug/Person.js
@@ -0,0 +1,35 @@
+﻿    // Constructor
+function Person(Name, status) {
+    // always initialize all instance properties
+    this.name = Name;
+    this.lastdate = [];
+    this.build = [];
+    this.builDateArr = [];
+    this.gravUrl = "";
+    this.status = status;
+}
+    // class methods
+Person.prototype = {
+    points: 0,
+    build: []
+};
+Person.prototype.addPoints = function (punt) {
+    this.points += punt;
+};
+Person.prototype.substractPoints = function (punt) {
+    this.points -= punt;
+};
+Person.prototype.organizeArray = function() {
+    var object = {};
+    while (this.build.length) {
+        var key = this.build.pop();
+        object[key] = this.lastdate.pop();
+    }
+    this.builDateArr.push( object);
+};
+Person.prototype.showPoints = function () {
+    console.log("Person: " + this.name + " has " + this.points + " points");
+
+};
+    // export the class
+module.exports = Person;
