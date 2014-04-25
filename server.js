d49d24a35b7b42bb7512e2809208b26824f57bc0
senne <senne.vandesompele@gmail.com>
2014-04-25 13:31:37 +0200

    debugging turned off

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post( '/', function ( req, res ) {
     //POST data to any listeners
     console.log( req['body']['build']['buildId'] );
     try {
-        points("19312" /*req['body']['build']['buildId']*/, function ( err, profile ) {
+        points(req['body']['build']['buildId'], function ( err, profile ) {
             profiles = profile;
             try {
                 redis.get( "obj", function ( er, data ) {
