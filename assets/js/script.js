e81f438bea73e1dca5941484fb8e22975eb00052
senne <senne.vandesompele@gmail.com>
2014-04-30 12:04:52 +0200

    if last build was successful but still les than 0 points snowing

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,15 +22,20 @@ socket.on('message', function(msg) {
     for (var name in result) {
         if (msg[name]['status'] == 'Failed') {
             if (msg[name]['points'] < 0) {
-                gridster.add_widget('<li class="new"><div class="playerlist"><div class="player"><div class="box"><div class="headshot"><img src=' + msg[name]['gravUrl'] + ' border="0" style="border: 1px solid black;"><img src="/img/snow.gif" style="position: absolute; top: 17px; left:44px; opacity:0.8;"></div><div class="details wide"><div class="det"><div class="name">' + msg[name]['name'] + '</div><div class="status">' + msg[name]['status'] + '</div></div><div class="badge font-effect-ice" >You&#8217;re cold</div></div></div><div class="box"><div class="points">' + msg[name]['points'] + '</div></div></div></div></li>', 2, 1, 4, 1);
+                gridster.add_widget('<li class="new"><div class="playerlist"><div class="player"><div class="box red"><div class="headshot"><img src=' + msg[name]['gravUrl'] + ' border="0" style="border: 1px solid black;"><img src="/img/snow.gif" style="position: absolute; top: 17px; left:44px; opacity:0.8;"></div><div class="details wide"><div class="det"><div class="name">' + msg[name]['name'] + '</div><div class="status">' + msg[name]['status'] + '</div></div><div class="badge font-effect-ice" >You&#8217;re cold</div></div></div><div class="box"><div class="points">' + msg[name]['points'] + '</div></div></div></div></li>', 2, 1, 4, 1);
             } else {
                 gridster.add_widget('<li class="new"><div class="playerlist"><div class="player"><div class="box red"><div class="headshot"><img src=' + msg[name]['gravUrl'] + ' border="0" style="border: 1px solid black; "></div><div class="details wide"><div class="name">' + msg[name]['name'] + '</div><div class="status">' + msg[name]['status'] + '</div></div></div><div class="box red"><div class="points">' + msg[name]['points'] + '</div></div></div></div></li>', 2, 1, 4, 1);
             }
         } else {
+
             if (msg[name]['streak5'] == true) {
                 gridster.add_widget('<li class="new"><div class="playerlist"><div class="player"><div class="box"><div class="headshot"><img src=' + msg[name]['gravUrl'] + ' border="0" style="border: 1px solid black;"><img src="/img/fire.gif" style="position: absolute; top: 17px; left:44px; opacity:0.5;"></div><div class="details wide"><div class="det"><div class="name">' + msg[name]['name'] + '</div><div class="status">' + msg[name]['status'] + '</div></div><div class="badge font-effect-fire-animation" >You&#8217;re on fire</div></div></div><div class="box"><div class="points">' + msg[name]['points'] + '</div></div></div></div></li>', 2, 1, 4, 1);
             } else {
-                gridster.add_widget('<li class="new"><div class="playerlist"><div class="player"><div class="box"><div class="headshot"><img src=' + msg[name]['gravUrl'] + ' border="0" style="border: 1px solid black;"></div><div class="details wide"><div class="name">' + msg[name]['name'] + '</div><div class="status">' + msg[name]['status'] + '</div></div></div><div class="box"><div class="points">' + msg[name]['points'] + '</div></div></div></div></li>', 2, 1, 4, 1);
+                if (msg[name]['points'] < 0) {
+                    gridster.add_widget('<li class="new"><div class="playerlist"><div class="player"><div class="box"><div class="headshot"><img src=' + msg[name]['gravUrl'] + ' border="0" style="border: 1px solid black;"><img src="/img/snow.gif" style="position: absolute; top: 17px; left:44px; opacity:0.8;"></div><div class="details wide"><div class="det"><div class="name">' + msg[name]['name'] + '</div><div class="status">' + msg[name]['status'] + '</div></div><div class="badge font-effect-ice" >You&#8217;re cold</div></div></div><div class="box"><div class="points">' + msg[name]['points'] + '</div></div></div></div></li>', 2, 1, 4, 1);
+                } else {
+                    gridster.add_widget('<li class="new"><div class="playerlist"><div class="player"><div class="box"><div class="headshot"><img src=' + msg[name]['gravUrl'] + ' border="0" style="border: 1px solid black;"></div><div class="details wide"><div class="name">' + msg[name]['name'] + '</div><div class="status">' + msg[name]['status'] + '</div></div></div><div class="box"><div class="points">' + msg[name]['points'] + '</div></div></div></div></li>', 2, 1, 4, 1);
+                }
             }
         }
     }
